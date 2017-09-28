69cc90d5a5e553b1bd2fa623a76f61d27215202a
saviodo5591 <jcharney95@gmail.com>
2017-09-28 05:32:01 -0500

    Initial Commit

--- src/app/challenges/challenges.component.ts
+++ src/app/challenges/challenges.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ChallengeCreationComponent } from '../challenge-creation/challenge-creation.component';
+import { DialogService } from 'ng2-bootstrap-modal';
 
 @Component({
   selector: 'app-challenges',
   templateUrl: './challenges.component.html',
-  styleUrls: ['./challenges.component.css']
+  styleUrls: ['../domain/domain.component.css', './challenges.component.css']
 })
 export class ChallengesComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogService: DialogService) {}
 
   ngOnInit() {
+  }
+
+  openChallengeCreation() {
+    let disposable = this.dialogService.addDialog(ChallengeCreationComponent, {}, {
+      closeByClickingOutside: true
+    });
   }
 
 }
