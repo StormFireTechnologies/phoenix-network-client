c89084c3e2b39cb53676340145cb29658c2a6c1a
saviodo5591 <jcharney95@gmail.com>
2017-09-28 05:32:35 -0500

    Added LoginService hook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { LoginService } from './login/login.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  styleUrls: ['./app.component.css'],
+  providers: [LoginService]
+  })
 export class AppComponent {
-  title = 'app';
+  title = 'Phoenix Network';
+
+  constructor(public auth: LoginService) {
+    auth.handleAuthentication();
+  }
 }
