007ea19dda770e81642828715eb99c921d5aa294
saviodo5591 <jcharney95@gmail.com>
2017-09-28 05:40:29 -0500

    Updated styleUrls

--- src/app/activity-search/activity-search.component.ts
+++ src/app/activity-search/activity-search.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'activity-search',
   templateUrl: './activity-search.component.html',
-  styleUrls: ['./activity-search.component.css']
+  styleUrls: ['../domain/domain.component.css', './activity-search.component.css']
 })
 export class ActivitySearchComponent implements OnInit {
 
