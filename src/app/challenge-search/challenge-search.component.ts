1ff06fcfa54bd8d760ae4dc5fbb488651f00bf83
saviodo5591 <jcharney95@gmail.com>
2017-09-28 05:38:51 -0500

    Initial Commit

---
+++ src/app/challenge-search/challenge-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'challenge-search',
+  templateUrl: './challenge-search.component.html',
+  styleUrls: ['../domain/domain.component.css', './challenge-search.component.css']
+})
+export class ChallengeSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
