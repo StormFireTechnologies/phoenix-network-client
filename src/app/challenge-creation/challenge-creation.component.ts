33ea4b58500305e88a8f76394ebcf422c8497d66
saviodo5591 <jcharney95@gmail.com>
2017-09-29 03:50:26 -0500

    Added challenge creation mutation

--- src/app/challenge-creation/challenge-creation.component.ts
+++ src/app/challenge-creation/challenge-creation.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { DialogComponent, DialogService } from 'ng2-bootstrap-modal';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { UUID } from 'angular2-uuid';
+
+const createChallenge =gql`
+  mutation createChallenge($challenge: ChallengeInput!) {
+    createChallenge(challenge: $challenge) {
+      id
+    }
+  }
+`;
 
 export interface confirmModal {
   challenge: any;
@@ -13,8 +24,9 @@ export interface confirmModal {
 export class ChallengeCreationComponent extends DialogComponent<confirmModal, boolean> implements confirmModal, OnInit {
 
   challenge: any;
+  uuid = UUID.UUID();
 
-  constructor(public dialogService: DialogService) {
+  constructor(public dialogService: DialogService, private apollo: Apollo) {
     super(dialogService);
   }
 
@@ -27,7 +39,29 @@ export class ChallengeCreationComponent extends DialogComponent<confirmModal, bo
   }
 
   createChallenge(challengeForm) {
-    console.log(challengeForm.value);
+    return this.apollo.mutate({
+      mutation: createChallenge,
+      variables: {
+        challenge: {
+          id: this.uuid,
+          title: challengeForm.value.title,
+          game: challengeForm.value.game,
+          creator: "auth0|597d8e5a497425796a831983",
+          createdAt: Date.now(),
+          timeStart: Date.now(),
+          timeEnd: "1 hour",
+          objective: challengeForm.value.objective,
+          goal: challengeForm.value.goal,
+          participants: challengeForm.value.participants,
+          placements: challengeForm.value.placements,
+          rewards: challengeForm.value.rewards,
+        },
+      },
+    }).subscribe(({ data }) => {
+      console.log(data);
+    }, (error) => {
+      console.log('There was an error creating the challenge: ', error);
+    });
   }
 
 }
